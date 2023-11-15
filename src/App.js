2a8827dcc278be0df4f4a483c88dac2d9bbb5c2e
Neetesh NG <neeteshng15@gmail.com>
2023-11-15 15:14:35 +0530

    Removed MDB React UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import {
-  MDBCard,
-  MDBCardBody,
-  MDBCardImage,
-  MDBCol,
-  MDBContainer,
-  MDBIcon,
-  MDBRow,
-  MDBTypography,
-} from "mdb-react-ui-kit";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 const apiKey = "3ee93d1a25d6522bc6ea1f5cd2f93fbd";
 
@@ -20,12 +10,14 @@ function App() {
 
   useEffect(() => {
     const fetchWeatherData = async () => {
-      try{
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`);
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`
+        );
 
         const data = await response.json();
         setWeatherData(data);
-      }catch (error) {
+      } catch (error) {
         console.error("Error while fetching Data : ", error);
       }
     };
@@ -34,38 +26,66 @@ function App() {
 
   const handleSearch = () => {
     setLocation(search);
-  }
+  };
 
   return (
     <div className="App">
-      <section className="vh-100" style={{ backgroundColor: "#f5f6f7" }}>
-      <MDBContainer className="h-100">
-        <MDBRow className="justify-content-center align-items-center h-100">
-          <MDBCol md="10" lg="8" xl="6">
-            <div className="mb-3">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter location..."
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-              />
-              <button className="btn btn-primary mt-2" onClick={handleSearch}>
-                Search
-              </button>
-            </div>
-            {weatherData && (
-              <MDBCard
-                className="bg-dark text-white"
-                style={{ borderRadius: "40px" }}
-              >
-              </MDBCard>
-            )}
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-    </section>
-
+      <section className="section-container">
+        <div className="body-container">
+              <div className="search-box">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter location..."
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+                <button className="search-button" onClick={handleSearch}>
+                  Search
+                </button>
+              </div>
+              {weatherData && (
+                <div
+                  className="icon-image"
+                  style={{ borderRadius: "40px" }}
+                >
+                  <div className="bg-image" style={{ borderRadius: "35px" }}>
+                    {/* Display weather image or icon */}
+                    {weatherData.weather && weatherData.weather.length > 0 && (
+                      <img
+                        src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+                        className="card-img"
+                        alt="weather"
+                      />
+                    )}
+                    <div
+                      className="mask"
+                    ></div>
+                  </div>
+                  <div className="weather-card">
+                    <p className="location-name">
+                      {weatherData.name},{" "}
+                      {weatherData.sys && weatherData.sys.country}
+                    </p>
+                    <p className="temperature">
+                      {weatherData.main && weatherData.main.temp}&deg;C
+                    </p>
+                    <p className="feels-like">
+                      Feels Like:{" "}
+                      <strong>
+                        {weatherData.main && weatherData.main.feels_like}&deg;C
+                      </strong>
+                    </p>
+                    {weatherData.weather && weatherData.weather.length > 0 && (
+                      <p className="weather-type">
+                        {weatherData.weather[0].main}
+                      </p>
+                    )}
+                  </div>
+                </div>
+              )}
+        </div>
+      </section>
     </div>
   );
 }
