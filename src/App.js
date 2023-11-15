74e91fa33d3ece7b1f356ab578a3aca5c1cdcca3
Neetesh NG <neeteshng15@gmail.com>
2023-11-15 14:45:26 +0530

    Added API to the App and Made functions to fetch the weather Data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const apiKey = "3ee93d1a25d6522bc6ea1f5cd2f93fbd";
+
 function App() {
+  const [location, setLocation] = useState("Juneau, Alaska, US");
+  const [weatherData, setWeatherData] = useState(null);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`);
+
+        const data = await response.json();
+        setWeatherData(data);
+      }catch (error) {
+        console.error("Error while fetching Data : ", error);
+      }
+    };
+    fetchWeatherData();
+  }, [location]);
+
+  const handleSearch = () => {
+    setLocation(search);
+  }
+
   return (
     <div className="App">
       
